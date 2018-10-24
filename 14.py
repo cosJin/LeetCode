81f6aa136d4ba6e2b927af6b8257a6f48ef921bd
Yu <cosJin@live.cn>
2018-10-24 10:26:22 +0800

    添加14题利用字符串排序的方法。

--- 14.py
+++ 14.py
@@ -19,5 +19,22 @@ class Solution:
                         return ""
                     else: return strs[0][0:i+1]
             if i == len(strs[0])-1:return strs[0]
-test = Solution()
-print(test.longestCommonPrefix(["f","f","f"]))
+class Solution2:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        
+        
+        if not strs: return ''
+				#since list of string will be sorted and retrieved min max by alphebetic order
+        s1 = min(strs)
+        s2 = max(strs)
+
+        for i, c in enumerate(s1):
+            if c != s2[i]:
+                return s1[:i] #stop until hit the split index
+        return s1
+test = Solution2()
+print(test.longestCommonPrefix(["fine","fimed","fifm"]))
