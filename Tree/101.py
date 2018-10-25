9ff5d1aff2e9791b07d42f2f018abb1b052f5275
Yu <cosJin@live.cn>
2018-10-25 15:59:40 +0800

    Qusetion 101:采用分辨正向反向遍历左右子树，然后比较遍历结果是否相等的方法，麻烦了。

---
+++ Tree/101.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root is None:return True
+        zzy = []
+        yzz = []
+        zzy = self.zzy(root.left)
+        yzz = self.yzz(root.right)
+        return zzy == yzz
+
+    def zzy(self,root):
+        result = []
+        if root is None: result.append(None)
+        elif root.left != None or root.right != None:
+            result.append(self.zzy(root.left))
+            result.append(root.val)
+            result.append(self.zzy(root.right))
+        else:result.append(root.val)
+        return result
+    
+    def yzz(self,root):
+        result = []
+        if root is None: result.append(None)
+        elif root.left != None or root.right != None:
+            result.append(self.yzz(root.right))
+            result.append(root.val)
+            result.append(self.yzz(root.left))
+        else:result.append(root.val)
+        return result
