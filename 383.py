ab1c9c515405b68564bfbf172c16fe71ec0ea80d
Yu <cosJin@live.cn>
2018-10-25 10:21:15 +0800

    Question:383

--- 383.py
+++ 383.py
@@ -5,4 +5,15 @@ class Solution:
         :type magazine: str
         :rtype: bool
         """
-        
+        # r = list(ransomNote)
+        # m = list(magazine)
+        r = ransomNote
+        m = magazine
+        for i in r:
+            if i in m:
+                # m.remove(i)
+                m = m.replace(i,'',1)  #相当于从字符串中删除i
+            else:return False
+        return True
+test = Solution()
+print(test.canConstruct('a','aba'))
