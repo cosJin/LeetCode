8bd7c0c86ebf33a50e2a7798c93a81e9231b77a1
Yu <cosJin@live.cn>
2018-10-29 20:03:01 +0800

    *Question 257:输出所有叶节点的路径。自己想的有点久，没有理清思路，最后参考的答案，希望以后自己做一遍。 注意：在函数中再定义一个函数则可以在内层函数中直接修改外层函数中的参数值，而不需要将该参数传入内部函数中。

--- Tree/257.py
+++ Tree/257.py
@@ -11,14 +11,33 @@ class Solution:
         :type root: TreeNode
         :rtype: List[str]
         """
+        result = []
+        def path(root,parentPath):
+            if root is None:return None
+            if not root.left and not root.right:
+                result.append(parentPath+str(root.val))
+            if root.left:
+                path(root.left,parentPath+str(root.val)+"->")
+            if root.right
+                path(root.right,parentPath+str(root.val)+"->")
+        path(root,"")
+        return result
+
+            
+class Solution2:
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
         parentPath = []
         return self.allPath(root,parentPath)
 
     def allPath(self,tree,parentPath):
         pathList = []
         if tree.left is None and tree.right is None:
-            path = parentPath.append(tree.val)  
-            pathList.append(path)               
+            path = parentPath.append(tree.val)   #[1,2,4]
+            pathList.append(path)                #[[1,2,4]]
         if tree.left:
             parentPath.append(tree.val)
             pathList += self.allPath(tree.left,parentPath)
