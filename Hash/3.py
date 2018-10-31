f0a658971a294960736ed87479ab7ee1d4335dc2
Yu <cosJin@live.cn>
2018-10-31 15:52:57 +0800

    Question 3:求最大不重复字符长度，自己完成的，但是较慢，需要改进。

--- Hash/3.py
+++ Hash/3.py
@@ -4,4 +4,20 @@ class Solution:
         :type s: str
         :rtype: int
         """
-        
+        len = 0
+        maxlen = 0
+        top = 0
+        dic = {}
+        for char in s:
+            while(char in dic):
+                dic.pop(s[top])
+                top += 1
+                len -= 1
+            dic[char] = 1
+            len += 1
+            maxlen = max(maxlen,len)
+        return maxlen
+text = Solution()
+print(text.lengthOfLongestSubstring("bbbbbbb"))
+            
+
