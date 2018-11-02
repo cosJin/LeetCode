1ecfa47066ac2677d98eea6ef3123d3ab31bfd7f
Yu <cosJin@live.cn>
2018-11-02 10:40:17 +0800

    new 138.py.

---
+++ Hash/138.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list with a random pointer.
+# class RandomListNode(object):
+#     def __init__(self, x):
+#         self.label = x
+#         self.next = None
+#         self.random = None
+
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: RandomListNode
+        :rtype: RandomListNode
+        """
+        copy = []
+        if head is None:return copy
+        else:
+            copy.append
+            while(head.next):
+
