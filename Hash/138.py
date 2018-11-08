cc89f9a0d47cf5054c847c96c9db353c803e6479
Yu <cosJin@live.cn>
2018-11-08 11:05:12 +0800

    Question138:对照着答案，自己总算写出来了，创建的字典是用来找到原始链表与新链表之间的对应关系的。若单纯的复制链表还是比较简单的。

--- Hash/138.py
+++ Hash/138.py
@@ -14,22 +14,25 @@ class Solution(object):
         node_map = {}
         if head is None: return None
         new_head = RandomListNode(head.label)
-        ptr = head.next
-        new_ptr = new_head
-        node_map.update({head: new_head})
-        # Set only next nodes
-        while ptr is not None:
-            new_node = RandomListNode(ptr.label)
-            new_ptr.next = new_node
-            new_ptr = new_node
-            node_map.update({ptr: new_ptr})
-            ptr = ptr.next            
-        # Set random pointers
-        ptr = head
-        new_ptr = new_head
-        while ptr is not None:
-            if ptr.random is not None:
-                new_ptr.random = node_map[ptr.random]
-            ptr = ptr.next
-            new_ptr = new_ptr.next
+        node_map.update({head:new_head})
+        pNew = new_head
+        p = head.next
+        while (p != None):
+            newNode = RandomListNode(p.label)
+            pNew.next = newNode
+            node_map.update({p:newNode})
+            p = p.next
+            pNew = newNode
+        p = head
+        pNew = new_head
+        while p is not None:
+            if p.random is not None:
+                pNew.random = node_map[p.random]
+                # pNew.random = p.random   #这样就错了。p.random是原始列表中的节点，需要利用map映射找到原始列表中节点对应的新列表的节点才可以。
+            p = p.next
+            pNew = pNew.next
         return new_head
+            
+
+
+   
