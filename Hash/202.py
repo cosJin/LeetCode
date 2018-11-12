f8f30554b3f48f83c5def64a96c6ac71f9f82020
Yu <cosJin@live.cn>
2018-11-12 13:47:29 +0800

    Question201:之前题目理解错了，想了挺久，但实际挺简单，所以要注意审题；同时注意python中数字默认是float，会有小数，只要整数的时候要加个int()

--- Hash/202.py
+++ Hash/202.py
@@ -4,3 +4,21 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
+        result = {}
+        sum = 0
+        while 1:
+            while n != 0:
+                m = int(n%10)
+                n = int(n/10)
+                sum += m*m
+            if sum not in result.keys():
+                result[sum]=1
+                n = sum
+                sum = 0
+                if n == 1:
+                    return True
+            else:return False
+test = Solution()
+print(test.isHappy(9))
+        
+
