29f8b606c56093fbe6765717cebd7c0c6b21afd1
Yu <cosJin@live.cn>
2018-11-14 16:51:19 +0800

    Q219完善，排除无相同元素的情况。会快很多。

--- Hash/219.py
+++ Hash/219.py
@@ -5,10 +5,16 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-        for d in range(k+1)[1:]:
-            for i in range(len(nums)-d):
-                if nums[i] == nums[i+d]:
+        dic = {}
+        if len(set(nums)) == len(nums):
+            return False
+        for i in range(len(nums)):
+            try :
+                dic[nums[i]]
+                if i-dic[nums[i]]<=k:
                     return True
+                else: dic[nums[i]] = i
+            except:dic[nums[i]] = i
         return False
 test = Solution()
-print(test.containsNearbyDuplicate([3,3],2))
+print(test.containsNearbyDuplicate([1,2,3,1,2,3],2))
