b32cbe65d2b0672a69d859d23082b508e4a48027
Yu <cosJin@live.cn>
2018-11-14 09:57:10 +0800

    204：写了个利用列表的方法，对于输入10000来说，运行时间貌似比dic快了一些，但仍然超时。

--- Hash/204.py
+++ Hash/204.py
@@ -5,12 +5,17 @@ class Solution(object):
         :rtype: int
         """
         # dic = dict(zip(list(range(n-3)+2),[1]*(n-2)))
-        dic = dict(zip(list(range(n)[2:]),[1]*(n-2)))
-        for i in range(int(n))[2:]:
-            for key in dic.keys():
-                if key%i == 0 and key/i!=1:
-                    dic[key] = 0
-        return list(dic.values()).count(1)
-        # return dic
+        # dic = dict(zip(list(range(n)[2:]),[1]*(n-2)))
+        # for i in range(n)[2:]:
+        #     for key in dic.keys():
+        #         if key%i == 0 and key/i!=1:
+        #             dic[key] = 0
+        # return list(dic.values()).count(1)
+        l = list(range(n))[2:]
+        for i in range(n)[2:]:
+            for element in l:
+                if element%i == 0 and element/i!=1:
+                    l.remove(element)
+        return len(l)
 test = Solution()
 print(test.countPrimes(10000))
