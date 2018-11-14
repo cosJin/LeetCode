919a86c59eb39d825a1b1b823c694a8c3afc19a0
Yu <cosJin@live.cn>
2018-11-14 11:17:22 +0800

    筛选法，不用循环到n-1，循环到根号n即可，利用切片批量修改值。只保存True or False可节约空间。

--- Hash/204.py
+++ Hash/204.py
@@ -3,19 +3,28 @@ class Solution(object):
         """
         :type n: int
         :rtype: int
-        """
-        # dic = dict(zip(list(range(n-3)+2),[1]*(n-2)))
+        # """
         # dic = dict(zip(list(range(n)[2:]),[1]*(n-2)))
-        # for i in range(n)[2:]:
+        # for i in range(int(n**0.5)+1)[2:]:
         #     for key in dic.keys():
         #         if key%i == 0 and key/i!=1:
-        #             dic[key] = 0
+        #             del dic[key]
         # return list(dic.values()).count(1)
-        l = list(range(n))[2:]
-        for i in range(n)[2:]:
-            for element in l:
-                if element%i == 0 and element/i!=1:
-                    l.remove(element)
-        return len(l)
+        # # l = list(range(n))[2:]
+        # for i in range(int(n**0.5)+1)[2:]:
+        #     for element in l:
+        #         if element%i == 0 and element/i!=1:
+        #             l.remove(element)
+        # return len(l)
+        if n<3: return 0 
+        l = [True]*n
+        l[0] = False
+        l[-1] = False
+        for i in range(int(n**0.5)+1)[2:]:
+            l[2*i-1:-1:i] = [False]*(len(l[2*i-1:-1:i]))
+        return l.count(True)
+        # return l
+
+
 test = Solution()
-print(test.countPrimes(10000))
+print(test.countPrimes(3))
--- try.py
+++ try.py
@@ -1,6 +1,10 @@
-n = 24
-d = list(range(n)[2:])
-a = list(range(22))
-dic = dict(zip(d,a))
-a = list(dic.keys())
-print(a.count(3))
+n = 10
+l = [True]*n
+l[0] = False
+l[-1] = False
+for i in range(int(n**0.5)+1)[2:]:
+    print(i)
+    l[2*i-1:-1:i] = [False]*(len(l[2*i-1:-1:i]))
+
+print(l.count(True))
+print(l)
