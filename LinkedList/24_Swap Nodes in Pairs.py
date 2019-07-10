e5b3c6dac5800dc58d4df17f893b29098b368730
yuxuejin@sogou <yuxuejin@sogou-inc.com>
2019-07-10 18:14:09 +0800

    参考了视频里的 pre, pre.next = self, head，这句有点秀，pre = self相当于生成了一个对象，作为头结点。此头结点不同于题目中的head。

--- LinkedList/24_Swap Nodes in Pairs.py
+++ LinkedList/24_Swap Nodes in Pairs.py
@@ -15,5 +15,13 @@ class Solution:
         cur.next,nex.next = nex.next,cur
         return a
         
+    def swapPairs(self, head: ListNode) -> ListNode:
+        pre, pre.next = self, head
+        while pre.next and pre.next.next:
+            a = pre.next
+            b = a.next
+            pre.next, b.next, a.next = b, a, b.next 
+            pre = a 
+        return self.next
 
         
