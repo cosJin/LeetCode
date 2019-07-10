e1735ed0340b46916e5e641154914660bfd578fb
yuxuejin@sogou <yuxuejin@sogou-inc.com>
2019-07-10 17:42:50 +0800

    根据206题中关于逗号连续赋值的启发，自己写的，需要注意的是，head中也有内容，不是空的。

---
+++ LinkedList/24_Swap Nodes in Pairs.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if head is None: return head
+        elif head.next is None: return head
+        cur,nex = head,head.next
+        a = nex
+        while nex.next is not None and nex.next.next is not None:
+            cur.next,nex.next,cur,nex = nex.next.next,cur,nex.next,nex.next.next
+        cur.next,nex.next = nex.next,cur
+        return a
+        
+
+        
