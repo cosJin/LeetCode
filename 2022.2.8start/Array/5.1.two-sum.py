64dc78664fc6d1a98381ab6aa103ee447c74246e
yu <cosJin@live.cn>
2022-02-21 23:02:57 +0800

    two sum

--- 2022.2.8start/Array/5.1.two-sum.py
+++ 2022.2.8start/Array/5.1.two-sum.py
@@ -1,2 +1,9 @@
 class Solution(object):
     def twoSum(self, nums, target):
+        res = {}
+        for n,v in enumerate(nums):
+            if target-v not in res:
+                res[v] = n
+            else:
+                return [res[target-v],n]
+print(Solution().twoSum([2,7,44,34],9))
