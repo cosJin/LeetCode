fb509fb5d79f308c1aeebdab56a6c1c25dad1ca7
Yu_at_ubuntu <cosJin@live.cn>
2022-05-12 22:35:26 +0800

    序列化和反序列化没有严格遍历顺序要求，能通过验证即可，验证方式题目中已用注释标记

--- 2022.2.8start/297.二叉树的序列化与反序列化.py
+++ 2022.2.8start/297.二叉树的序列化与反序列化.py
@@ -11,15 +11,29 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
 class Codec:
-
     def serialize(self, root):
         """Encodes a tree to a single string.
         
         :type root: TreeNode
         :rtype: str
         """
+        if not root:
+            return ''
+        
+        arr = []
+        queue = [root]
+        while queue:
+            node = queue.pop(0)
+            arr.append(str(node.val) if node else 'null')
+            if node:
+                queue.append(node.left)
+                queue.append(node.right)
+                
+        while arr[-1] == 'null':
+            arr.pop()
+        
+        return ','.join(arr)
         
 
     def deserialize(self, data):
@@ -28,8 +42,36 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
+        if not data: return None
+        queue = list(map(lambda x: None if x=='null' else int(x), data.split(',')))
+        Tree_queue = []
+        root = None
         
-
+        while queue:
+            val = queue.pop(0)
+            if not root:
+                root = TreeNode(val)
+                Tree_queue.append((root, 'left'))
+                continue
+            
+            node, side = Tree_queue.pop(0)
+            
+            if side == 'left':
+                if val is not None:
+                    node.left = TreeNode(val)
+                    Tree_queue.append((node.left, 'left'))
+                Tree_queue.insert(0, (node, 'right'))
+                
+            
+            elif side == 'right':
+                if val is not None:
+                    node.right = TreeNode(val)
+                    Tree_queue.append((node.right, 'left'))
+        return root
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
 # deser = Codec()
--- 2022.2.8start/tmp.py
+++ 2022.2.8start/tmp.py
@@ -1,3 +1,2 @@
-a=[1,2,3,3]
-for n,i in enumerate(a):
-    print(n,i)
+a='[1,2,3,3]'
+print(list(a))
