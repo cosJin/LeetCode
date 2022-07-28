c90c6cf5747df9b48f61e0150b2db0fc0c5c84db
Yu_at_ubuntu <cosJin@live.cn>
2022-07-28 23:23:33 +0800

    括号生成和 二叉树的层序遍历

--- 2022.2.8start/102.二叉树的层序遍历.py
+++ 2022.2.8start/102.二叉树的层序遍历.py
@@ -17,5 +17,38 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
+        if root is None:return []
+        tree = [[root]]
+        res = []
+        for level in tree:
+            if level == []:return res
+            res.append([])
+            tree.append([])
+            for node in level:
+                if node is not None:
+                    res[-1].append(node.val)
+                    if node.left is not None:
+                        tree[-1].append(node.left)
+                    if node.right is not None:
+                        tree[-1].append(node.right)
+                        
+class Solution:   #不用像上面那么巧妙，直接定义一个当前，一个待遍历节点就行，然后将结果存一个。
+    def levelOrder(self, root):
+        if root is None:return []
+        now,nxt = [root],[]
+        res = [] 
+        while now != []:
+            res.append([])
+            for node in now:
+                res[-1].append(node.val)
+                if node.left is not None:nxt.append(node.left)
+                if node.right is not None:nxt.append(node.right)
+            now = nxt[:]  #浅拷贝 注意
+            nxt = []
+        return res
+
+
+
+
 # @lc code=end
 
