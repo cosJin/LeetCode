8c6b17fa236f1e33bd2c31d62b08b68400bc3dbb
Yu_at_ubuntu <cosJin@live.cn>
2022-10-19 23:26:59 +0800

    单词搜索ii

--- 2022.2.8start/212.单词搜索-ii.py
+++ 2022.2.8start/212.单词搜索-ii.py
@@ -3,45 +3,51 @@
 #
 # [212] 单词搜索 II
 #
+from matplotlib import collections
+
 
 # @lc code=start
 class Solution(object):
+
+    def _dfs(self,board,i,j,cur_word,cur_dict):
+            cur_word += board[i][j]
+            cur_dict = cur_dict[board[i][j]]
+            if self.END_OF_WORD in cur_dict:
+                self.result.add(cur_word)
+            tmp,board[i][j] = board[i][j],'@'
+            for k in range(4):
+                x,y = i + self.dx[k],j + self.dy[k]
+                if 0 <= x<self.m and 0<=y <self.n and board[x][y]!='@' and board[x][y] in cur_dict:
+                    self._dfs(board,x,y,cur_word,cur_dict)
+            board[i][j] = tmp
+
     def findWords(self, board, words):
         """
         :type board: List[List[str]]
         :type words: List[str]
         :rtype: List[str]
         """
-        
+        self.dx = [-1,1,0,0]
+        self.dy = [0,0,-1,1]
+        self.END_OF_WORD = "#"
 
-        def helper(word1,board1,m,n):
-            if word1 == []:return True
-            if m-1 >= 0 and board1[m-1][n] == word1[0]:
-                board1[m-1][n] = 0
-                b = board1[:]
-                return helper(word1[1:],b,m-1,n)
-            if n-1 >= 0 and board1[m][n-1] == word1[0]:
-                board1[m][n-1] = 0
-                b = board1[:]
-                return helper(word1[1:],b,m,n-1)
-            if m+1 <=len(board1)-1 and board1[m+1][n] == word1[0]:
-                board[m+1][n] = 0
-                b = board1[:]
-                return helper(word1[1:],b,m-1,n)
-            if n+1 <=len(board1[0])-1 and board1[m][n+1] == word1[0]:
-                board[m][n+1] = 0
-                b = board1[:]
-                return helper(word1[1:],b,m,n+1)
+        if not board or not board[0]:return []
+        if not words:return []
+        self.result = set()
 
-        res = []
+        #构建ｔｒｉｅ
+        root = collections.defaultdict()
         for word in words:
-            tmp = board[:]
-            for mm in range(len(tmp)):
-                for nn in range(len(tmp[0])):
-                    if tmp[mm][nn] == word[0]:
-                        if helper(word,tmp,mm,nn):
-                            res.append(word)
-                            continue
-        return res
+            node = root
+            for char in word:
+                node = node.setdefault(char,collections.defaultdict())
+            node[self.END_OF_WORD] = self.END_OF_WORD
+
+        self.m,self.n = len(board),len(board[0])
+        for i in range(self.m):
+            for j in range(self.n):
+                if board[i][j] in root:
+                    self._dfs(board,i,j,"",root)
+        return list(self.result)
 # @lc code=end
 
